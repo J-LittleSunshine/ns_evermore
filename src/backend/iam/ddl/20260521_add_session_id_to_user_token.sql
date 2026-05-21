bc0829154fab63eae4ea43a164f0749c51f80154
nongqingfu <nongqingfu@dechengtech.com.cn>
2026-05-21 09:51:59 +0800

    移除添加session_id的sql文件

--- src/backend/iam/ddl/20260521_add_session_id_to_user_token.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE iam_user_token
-    ADD COLUMN session_id BIGINT UNSIGNED NULL COMMENT '会话ID' AFTER user_id,
-    ADD KEY idx_ut_session_id (session_id),
-    ADD CONSTRAINT fk_ut_session
-        FOREIGN KEY (session_id) REFERENCES iam_user_session (id);
