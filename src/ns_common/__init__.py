a4c0412ddfcde401ac45b2e3c5c97eb1a86bb192
nongqingfu <jf1453882193@gmail.com>
2026-06-23 14:22:43 +0800

    fix(common): isolate shared logging from Django loggers

--- src/ns_common/__init__.py
+++ src/ns_common/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from ns_common.config import NS_CONFIG_FILE_PATH, NS_ENV, ns_config
+from ns_common.logging import configure_ns_logging, get_logger
 from ns_common.paths import DATA_DIR, ETC_DIR, LOG_DIR, ROOT_DIR, SQL_DIR, TMP_DIR
 
 if TYPE_CHECKING:
@@ -20,6 +21,8 @@ __all__ = [
     "SQL_DIR",
     "TMP_DIR",
     "ns_config",
+    "configure_ns_logging",
+    "get_logger",
 ]
 
 __version__ = "0.0.1"
