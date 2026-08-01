d981da571a8b7260b35fe2aed17f390ac2abbf9c
Ns <jf1453882193@gmail.com>
2026-08-01 14:13:38 +0800

    重构

--- src/ns_backend/backend/urls.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import annotations
-
-from backend.app_loader import build_urlpatterns
-from ns_common import ns_config
-
-urlpatterns = build_urlpatterns(ns_config.backend.installed_apps)
--- src/ns_runtime/delivery/persistence.py
+++
@@ -1,4 +0,0 @@
-"""Compatibility exports for the runtime-level delivery persistence contract."""
-
-from ns_runtime.delivery_persistence import *
-from ns_runtime.delivery_persistence import __all__
